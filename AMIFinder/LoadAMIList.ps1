aa1e434394a07e6d22885f59689012823c2b505e
Jeremy Wu <jeremy24wu@gmail.com>
2019-08-11 16:33:27 +1000

    Find AMIs with descriptions

---
+++ AMIFinder/LoadAMIList.ps1
@@ -0,0 +1,17 @@
+﻿Para{
+    string profileName
+    string awsRegion
+}
+
+if(!Get-Module Setup-Environment.pms1){
+    Import-Module Setup-Environment.pms1
+}
+
+Set-AWSCredentials -StoredCredentials profileName
+
+Set-DefaultAWSRegion -Region awsRegion
+
+
+$blah = Get-EC2Image -Owner self
+
+$blah | ConvertTo-Json -depth 100 | Out-File ".\file.json"

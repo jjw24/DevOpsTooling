55868e338d300ab0368783ea39629809495e1ad0
Jeremy Wu <jeremy24wu@gmail.com>
2019-08-14 08:26:55 +1000

    add logging for PS scripts

--- AMIFinder/Wox/Plugins/Wox.Plugin.AMIFinder/Commands/LoadAMIList.ps1
+++ AMIFinder/Wox/Plugins/Wox.Plugin.AMIFinder/Commands/LoadAMIList.ps1
@@ -3,6 +3,8 @@
     [string] $awsRegion
 )
 
+$Error.Clear()
+
 if(!(Get-Module SetupEnvironmentAWS)){
 	Import-Module .\SetupEnvironmentAWS -WarningAction SilentlyContinue
 }
@@ -18,3 +20,7 @@ Set-AWSCredentials -StoredCredentials $profileName
 Set-DefaultAWSRegion -Region $awsRegion
 
 ConvertTo-Json -InputObject @(Get-EC2Image -Owner self | Select -Property Name, Description, CreationDate)
+
+if($Error.Count -gt 0){
+    $Error | Out-File ".\Logs\PSRuntimeErrors.txt"
+}

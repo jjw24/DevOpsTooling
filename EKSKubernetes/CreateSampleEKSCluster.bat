4578e9b04c555ac45c8c4f684a882387041f3a4a
Jeremy Wu <jeremy24wu@gmail.com>
2019-08-15 22:58:55 +1000

    Update EKSKube script

--- EKSKubernetes/CreateSampleEKSCluster.bat
+++ EKSKubernetes/CreateSampleEKSCluster.bat
@@ -2,9 +2,9 @@
 REM Currently to get it working, need to provide AdministratorAccess to the IAM user. Need to investigate further to scale down the access to specifics.
 
 
-choco upgrade awscli
+REM choco upgrade awscli
 
-choco upgrade eksctl aws-iam-authenticator -y
+REM choco upgrade eksctl aws-iam-authenticator -y
 
 aws --version
 
@@ -15,6 +15,6 @@ aws configure
 
 pause
 
-REM eksctl create cluster --name prod --version 1.13 --nodegroup-name standard-workers --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 4 --node-ami auto
+eksctl create cluster --name prod --version 1.13 --nodegroup-name standard-workers --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 4 --node-ami auto
 
 REM eksctl delete cluster --region=ap-southeast-2 --name prod

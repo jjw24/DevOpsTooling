e85ba25c00268496d708f0cdabc668af44a37420
Jeremy Wu <jeremy24wu@gmail.com>
2023-04-05 14:08:09 +1000

    Update new container cmds.sh

--- Docker/new container cmds.sh
+++ Docker/new container cmds.sh
@@ -1,5 +1,6 @@
-# pull and start python 3.9.5 container with alpine base image
+# pull and start container with interactive terminal
 docker run -it --name python3 --entrypoint=sh python:3.9.5-alpine
+docker run -it centos /bin/bash
 
 # start container with the available interactive terminal
 docker start -i python3
